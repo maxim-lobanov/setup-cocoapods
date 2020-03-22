e985cfef6b70e2cbe451960734918274a8eea692
Maxim Lobanov <v-malob@microsoft.com>
2020-03-22 15:19:34 +0300

    Remove debug lines

--- src/installer.ts
+++ src/installer.ts
@@ -18,7 +18,6 @@ export class CocoapodsInstaller {
         // Remove pre-installed version of Cocoapods
         exec.exec("gem", ["uninstall", "cocoapods", "--all", "--executables"]);
 
-        core.info("debug");
         // Install new version of Cocoapods
         const versionArguments = (versionSpec === "latest") ? [] : ["-v", versionSpec];
         await exec.exec("gem", ["install", "cocoapods", ...versionArguments, "--no-document"]);
